c817da5f8f4e2f56da9220745beffa0297c4ff70
Grace Abian <graceabian10@gmail.com>
2022-05-04 15:14:10 +0800

    done mongoDb setup

--- app.js
+++ app.js
@@ -7,12 +7,17 @@ const dotenv = require("dotenv");
 //[SECTION] Environtment Variables Setup
 dotenv.config();
 const port = process.env.PORT;
+const credentials = process.env.MONGO_URL;
 
 //[SECTION] Sever Setup
 const app = express();
 	app.use(express.json());
 
 //[SECTION] Database Connect
+mongoose.connect(credentials);
+const db = mongoose.connection;
+	db.once('open', () => console.log(`Connected to MongoDb Atlas`))
+
 //[SECTION] Server Routes
 //[SECTION] Server Response
 
