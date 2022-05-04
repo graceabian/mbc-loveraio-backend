aecfd12b7188937e9eb41837f45a7d246ac92a33
Grace Abian <graceabian10@gmail.com>
2022-05-04 15:27:18 +0800

    done with user model

--- model/user.js
+++ model/user.js
@@ -1,1 +1,23 @@
-user.js
+//[SECTION] Dependencies and Modules
+const mongoose = require("mongoose");
+
+//[SECTION] Schema
+const userSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: [true, "Email is Required"]
+	},
+	password: {
+		type: String,
+		required: [true, "Password is Required"]
+	},
+	isAdmin: {
+		type: Boolean,
+		required: false
+	}
+
+});
+
+//[SECTION] Model
+module.exports = mongoose.model("User, userSchema");
+
